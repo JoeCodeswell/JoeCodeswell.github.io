39a69edeb306030863c47d7520195fd8bcf7b4e7
Joe Dorocak <joecodeswell@gmail.com>
2019-04-10 09:23:53 -0700

    190410_09_23_01_Wed styling looks pretty good. upload to github

---
+++ mdSxHiLite.js
@@ -0,0 +1,19 @@
+/**
+ * [Syntax highlighting](https://www.npmjs.com/package/markdown-it#syntax-highlighting)
+ * 
+ */
+
+var hljs = require('highlight.js'); // https://highlightjs.org/
+ 
+// Actual default values
+var md = require('markdown-it')({
+  highlight: function (str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return hljs.highlight(lang, str).value;
+      } catch (__) {}
+    }
+ 
+    return ''; // use external default escaping
+  }
+});
